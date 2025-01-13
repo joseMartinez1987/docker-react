8fdb7f37a5ce0eeba8edd619442b3835c1d947a1
Jose Martinez <josemanuel.martinez@cencosud.cl>
2025-01-13 13:20:25 -0300

    change text

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <p>Learning docker container</p>
+        <p>Learning docker container on Elastic Beanstalk</p>
         <a
           className="App-link"
           href="https://reactjs.org"
